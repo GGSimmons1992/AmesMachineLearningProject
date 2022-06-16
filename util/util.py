86d75533647d13f69221ea39193943e92840a6f5
Gary Simmons <gary.simmons@colorado.edu>
2022-06-15 21:18:16 -0500

    add dummies, train-test-split, and ISUDistance function

--- util/util.py
+++ util/util.py
@@ -1,5 +1,12 @@
 import numpy as np
 import pandas as pd
+import json
+import geopy
+from geopy import GoogleV3
+from geopy.extra.rate_limiter import RateLimiter
+from geopy.distance import great_circle
+
+amesRealEstate = pd.read_csv('../data/Ames_Real_Estate_Data.csv')
 
 def markOutliers(values):
     if (type(values)==list):
@@ -13,4 +20,20 @@ def markOutliers(values):
 def removeOutliers(df):
     for col in df.columns:
         df = df[markOutliers(df[col]) == False]
+    return df
+
+def returnDFWithISUDistance(df):
+    with open('../data/googleApiKey.json') as d:
+        googleKeyDictionary = json.load(d)
+        apiKey = googleKeyDictionary['apikey']
+    locator = GoogleV3(api_key=apiKey)
+    geocode = RateLimiter(locator.geocode, min_delay_seconds=1.5)
+    ISU = geocode('Iowa State University, Ames, USA').point[0:2]
+    pids = df['PID'].to_frame()
+    pidMapRefNoMerge = pids.merge(amesRealEstate[['MapRefNo','Prop_Addr']],
+           how='left',left_on='PID',right_on='MapRefNo')
+    locations = [geocode(addr+', Ames,IA, USA').point[0:2] if type(addr)==str else None for addr in pidMapRefNoMerge['Prop_Addr']]
+    pidMapRefNoMerge['ISUDistance'] = pd.Series([great_circle(house,ISU).mi for house in locations])
+    df = df.merge(pidMapRefNoMerge[['MapRefNo','ISUDistance']],
+           how='left',left_on='PID',right_on='MapRefNo')
     return df
