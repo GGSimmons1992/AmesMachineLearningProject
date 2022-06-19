a19570e1173e6e1e51bf159569040c7e9f3d59ab
Gary Simmons <gary.simmons@colorado.edu>
2022-06-19 15:33:27 -0500

    replace nans, expand outlier params, and perform gridsearch

--- util/util.py
+++ util/util.py
@@ -20,8 +20,8 @@ def markOutliers(values):
         values = np.array(values)
     sd = np.std(values)
     xbar = np.mean(values)
-    datamin = xbar - ( 3 * sd )
-    datamax = xbar + ( 3 * sd )
+    datamin = xbar - ( 26 * sd )
+    datamax = xbar + ( 26 * sd )
     return ((values < datamin) | (values > datamax))
 
 def removeOutliers(df):
@@ -51,4 +51,9 @@ def getLocation(address):
     except BaseException:
         return None
 
- 
+def replaceNansWithTrainingDataValues(df):
+    with open('../data/trainNanReplacementValuesDictionary.json') as d:
+        trainNanReplacementValuesDictionary = json.load(d)
+    for col in df.columns:
+        df[col] = df[col].fillna(trainNanReplacementValuesDictionary[str(col)])
+    return df
